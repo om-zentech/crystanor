4ee5707e56f49715c83033b77b0c3dbcf7d84346
steffy-jk <steffy.khristi@zentechsolutions.com>
2026-05-06 19:25:26 +0530

    [ Fix ] added health check API

--- crystanor/crystanor/urls.py
+++ crystanor/crystanor/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+
+def health_check(request):
+    return HttpResponse("OK", status=200)
+
 
 urlpatterns = [
+    path('', health_check, name='health'),
     path('admin/', admin.site.urls),
     path('api/',include('enquiries.urls')),
 ]
